b4913e6353f72052f3421c05cb92f0da907222a9
Simon Lindgren <simon.g.lindgren@gmail.com>
2020-02-04 15:08:15 +0100

    Implemented turnserver client

--- go/go.mod
+++ go/go.mod
@@ -5,5 +5,5 @@ go 1.13
 require (
 	github.com/CzarSimon/httputil v0.0.0-20200202200343-0e43a0091012
 	github.com/opentracing/opentracing-go v1.1.0
-	github.com/rtcheap/dto v0.0.0-20200202200637-cca360490513
+	github.com/rtcheap/dto v0.0.0-20200204002519-a74f8e07a2a7 // indirect
 )
--- go/turnserver/turnserver.go
+++ go/turnserver/turnserver.go
@@ -2,16 +2,19 @@ package turnserver
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/CzarSimon/httputil/client"
 	"github.com/CzarSimon/httputil/jwt"
 	"github.com/opentracing/opentracing-go"
+	"github.com/rtcheap/dto"
 )
 
 // Client client interface for interacting with the turn-server session api.
 type Client interface {
-	Register(ctx context.Context) error
-	GetStatistics(ctx context.Context) error
+	Register(ctx context.Context, baseURL string, session dto.Session) error
+	Unregister(ctx context.Context, baseURL, userID string) error
+	GetStatistics(ctx context.Context, baseURL string) error
 }
 
 // NewClient creates a new client using the default implementatino.
@@ -22,6 +25,7 @@ func NewClient(httpClient client.Client) Client {
 	if httpClient.UserAgent == "" {
 		httpClient.UserAgent = "turnserver/restClient"
 	}
+	httpClient.BaseURL = ""
 
 	return &restClient{
 		http: httpClient,
@@ -32,18 +36,51 @@ type restClient struct {
 	http client.Client
 }
 
-func (c *restClient) Register(ctx context.Context) error {
-	span, _ := opentracing.StartSpanFromContext(ctx, "turnserver.restClient.Register")
+func (c *restClient) Register(ctx context.Context, baseURL string, session dto.Session) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "turnserver.restClient.Register")
 	defer span.Finish()
+
+	url := fmt.Sprintf("%s/v1/sessions", baseURL)
+	err := c.http.Post(ctx, url, session, nil)
+	if err != nil {
+		err = fmt.Errorf("failed to register %s. %w", session, err)
+		span.LogFields(tracelog.Bool("success", false), tracelog.Error(err))
+		return err
+	}
 
 	span.LogFields(tracelog.Bool("success", true))
 	return nil
 }
 
-func (c *restClient) GetStatistics(ctx context.Context) error {
-	span, _ := opentracing.StartSpanFromContext(ctx, "turnserver.restClient.GetStatistics")
+func (c *restClient) Unregister(ctx context.Context, baseURL, userID string) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "turnserver.restClient.Unregister")
 	defer span.Finish()
+
+	url := fmt.Sprintf("%s/v1/sessions/%s", baseURL, userID)
+	err := c.http.Delete(ctx, url, nil)
+	if err != nil {
+		err = fmt.Errorf("failed to unregister user(id=%s). %w", userID, err)
+		span.LogFields(tracelog.Bool("success", false), tracelog.Error(err))
+		return err
+	}
 
 	span.LogFields(tracelog.Bool("success", true))
 	return nil
+}
+
+func (c *restClient) GetStatistics(ctx context.Context, baseURL string) (dto.SessionStatistics, error) {
+	span, _ := opentracing.StartSpanFromContext(ctx, "turnserver.restClient.GetStatistics")
+	defer span.Finish()
+
+	var stats dto.SessionStatistics
+	url := fmt.Sprintf("%s/v1/sessions/statistics", baseURL)
+	err := c.http.Get(ctx, url, &stats)
+	if err != nil {
+		err = fmt.Errorf("failed to retrieve turn-server session statistics. %w", err)
+		span.LogFields(tracelog.Bool("success", false), tracelog.Error(err))
+		return dto.SessionStatistics{}, err
+	}
+
+	span.LogFields(tracelog.Bool("success", true))
+	return stats, nil
 }
